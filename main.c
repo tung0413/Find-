4cab1378bfe771c2e5c52c8e96a199e7ca6def7b
tung0413 <46188799+tung0413@users.noreply.github.com>
2019-05-19 21:44:05 +0800

    Update main.c

--- main.c
+++ main.c
@@ -16,7 +16,8 @@ double side3 (double hypotenuse, double side)
 double area(double i)
 {
     double side=side3(2,2/i),base1=2,ans=0;
-    for(double j=0;j<i;j++){
+    for(double j=0;j<i;j++)
+    {
         ans+=trapezoid(base1,side,2/i);
         base1=side;
         side=side3(2,(j+2)*2/i);
@@ -27,10 +28,12 @@ double area(double i)
 int main()
 {
     double ans=0,ans2;
-    for(double i=2;;i=i*2){
+    for(double i=2;;i=i*2)
+    {
             ans2=ans;
             ans=area(i);
-            if(ans-ans2<0.00001){
+            if(ans-ans2<0.00001)
+            {
                 printf("%lf",ans);
                 break;
             }
